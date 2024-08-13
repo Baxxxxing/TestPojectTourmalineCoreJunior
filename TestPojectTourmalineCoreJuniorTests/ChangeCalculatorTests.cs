0e3462ff11919264adcc60424999e581ff1381ec
Baxxxxing <renev-maksimka95@mail.ru>
2024-08-13 23:35:23 +0500

    2 more tests were written to check the operation of the functional part. Both newly written tests ended with the status "Failed" because the "GetChange" method does not provide for checking for the absence of any of the denominations.

--- TestPojectTourmalineCoreJuniorTests/ChangeCalculatorTests.cs
+++ TestPojectTourmalineCoreJuniorTests/ChangeCalculatorTests.cs
@@ -22,5 +22,29 @@ namespace TestPojectTourmalineCoreJunior.Tests
 
             Assert.AreEqual("Для сдачи используйте следующие монеты: 50р x 1 10р x 2 2р x 1", result);
         }
+
+        [TestMethod()]
+        public void TestNotEnoughCoins()
+        {
+            int[] nominal = { 100, 50, 10, 5, 2, 1 };
+            int[] countNominal = { 1, 1, 2, 0, 1, 1 };
+            int change = 74;
+
+            string result = ChangeCalculator.GetChange(nominal, countNominal, change);
+
+            Assert.AreEqual("Невозможно вернуть сдачу, используйте кредитную карту.", result);
+        }
+
+        [TestMethod()]
+        public void TestChangeNotAvailable()
+        {
+            int[] nominal = { 100, 50, 10, 5, 2, 1 };
+            int[] countNominal = { 1, 1, 0, 0, 0, 0 };
+            int change = 37;
+
+            string result = ChangeCalculator.GetChange(nominal, countNominal, change);
+
+            Assert.AreEqual("Невозможно вернуть сдачу, используйте кредитную карту.", result);
+        }
     }
 }
