6871ea3b23633fd7422b8acfc607c3b15156999c
Baxxxxing <renev-maksimka95@mail.ru>
2024-08-13 23:33:18 +0500

    The "TestExactChangeAvailable" test was written, checking the functional part of the solution, but the project was not compiled, since the functional part is missing.

--- TestPojectTourmalineCoreJuniorTests/ChangeCalculatorTests.cs
+++ TestPojectTourmalineCoreJuniorTests/ChangeCalculatorTests.cs
@@ -12,9 +12,15 @@ namespace TestPojectTourmalineCoreJunior.Tests
     public class ChangeCalculatorTests
     {
         [TestMethod()]
-        public void GetChangeTest()
+        public void TestExactChangeAvailable()
         {
-            Assert.Fail();
+            int[] nominal = { 100, 50, 10, 5, 2, 1 };
+            int[] countNominal = { 1, 1, 2, 0, 5, 10 };
+            int change = 72;
+
+            string result = ChangeCalculator.GetChange(nominal, countNominal, change);
+
+            Assert.AreEqual("Для сдачи используйте следующие монеты: 50р x 1 10р x 2 2р x 1", result);
         }
     }
 }
