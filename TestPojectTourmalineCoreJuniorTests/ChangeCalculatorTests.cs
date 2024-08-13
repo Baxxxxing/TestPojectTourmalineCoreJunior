d6e91a721ebe530613f479563e204811c21d661d
Baxxxxing <renev-maksimka95@mail.ru>
2024-08-13 23:32:30 +0500

    A solution has been created containing the project "TestPojectTourmalineCoreJunior" responsible for the implementation of the functional part, as well as the project "TestPojectTourmalineCoreJuniorTests" responsible for testing the functional part.

---
+++ TestPojectTourmalineCoreJuniorTests/ChangeCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestPojectTourmalineCoreJunior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPojectTourmalineCoreJunior.Tests
+{
+    [TestClass()]
+    public class ChangeCalculatorTests
+    {
+        [TestMethod()]
+        public void GetChangeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
